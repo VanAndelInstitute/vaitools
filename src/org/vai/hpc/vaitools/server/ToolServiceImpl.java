f2e011923c24743bb6689f177c42dedf5cf975cb
zack <zack@192.168.1.114>
2016-11-07 23:45:45 -0500

    initial working rought version

--- src/org/vai/hpc/vaitools/server/ToolServiceImpl.java
+++ src/org/vai/hpc/vaitools/server/ToolServiceImpl.java
@@ -23,26 +23,46 @@ public class ToolServiceImpl extends RemoteServiceServlet implements ToolService
 	@Override
 	public ArrayList<Tool> getModel(String search)
 	{
-		ArrayList<Tool> models = new ArrayList<Tool>();
-		Properties prop = new Properties();
+		ArrayList<Tool> tools = new ArrayList<Tool>();
 		try
 		{
 			File folder = new File("tools");
 			Charset charset = Charset.forName("ISO-8859-1");
 			for (final File fileEntry : folder.listFiles()) 
 			{
+				Tool t = new Tool();
 				System.out.println(fileEntry.getName());
 				List<String> lines = Files.readAllLines(Paths.get(fileEntry.getPath()), charset);
+				Boolean newEntry=true;
+				String header = "";
+				String content = "";
 				for (String line : lines) 
 				{
-					System.out.println(line);
+					if(newEntry && line.length() > 1)
+					{
+						header = new String(line);
+						content = "";
+						newEntry=false;
+					}
+					else if(line.length()>1)
+					{
+						content += line;
+					}
+					else if(content.length() > 1 && header.length() > 1)
+					{
+						t.getTextSections().put(header, content);
+						newEntry=true;
+						header="";
+						content="";
+					}
 			    }
+				tools.add(t);
 			}
 			
 		} catch (Exception e)
 		{
 			e.printStackTrace();
 		}
-		return models;
+		return tools;
 	}
 }
